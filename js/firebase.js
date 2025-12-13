6e98c9570dbedb2a1ba9e47d030fce7a6914477c
tresor676 <habimanatresor676@gmail.com>
2025-12-13 20:16:50 +0200

    Fix formatting and add database URL in firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,7 +5,11 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase
 const firebaseConfig = {
   apiKey: "AIzaSyC2WG-135oKY_s6xf5-nBBhxncNV9UayQ0",
   authDomain: "tresor-ae58e.firebaseapp.com",
+  databaseURL: "https://tresor-ae58e-default-rtdb.firebaseio.com",
   projectId: "tresor-ae58e",
+  storageBucket: "tresor-ae58e.firebasestorage.app",
+  messagingSenderId: "835562519447",
+  appId: "1:835562519447:web:e4032e73f7601ba3c522d2"
 };
 
 export const app = initializeApp(firebaseConfig);
