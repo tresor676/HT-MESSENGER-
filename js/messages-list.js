8426dc585ecd32c875d066c8410764c6f8afcd6d
tresor676 <habimanatresor676@gmail.com>
2025-12-17 16:26:05 +0200

    Update messages-list.js

--- js/messages-list.js
+++ js/messages-list.js
@@ -1,11 +1,9 @@
 import { auth, db } from "./firebase.js";
 import { ref, onChildAdded } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-/* AFFICHER LES MESSAGES D’UNE CONVERSATION */
 export function loadMessages(convId, container) {
   auth.onAuthStateChanged(user => {
     if (!user) return;
-
     const me = user.uid;
     container.innerHTML = "";
 
@@ -14,11 +12,17 @@ export function loadMessages(convId, container) {
       const div = document.createElement("div");
       div.className = msg.author === me ? "msg-right" : "msg-left";
 
+      // Déterminer état
+      let statusDisplay = "";
+      if (msg.status[me] === "sent") statusDisplay = "✔";
+      else if (msg.status[me] === "received") statusDisplay = "✔✔";
+      else if (msg.status[me] === "read") statusDisplay = "✔✔ (lu)";
+
       div.innerHTML = `<p>${msg.content}</p>
-                       <small>${msg.status}</small>`;
+                       <small>${statusDisplay}</small>`;
 
       container.appendChild(div);
-      container.scrollTop = container.scrollHeight; // scroll auto
+      container.scrollTop = container.scrollHeight;
     });
   });
 }
