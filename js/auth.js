d5d7a3872fc792a3b78211c05f9a49de0d58db5c
tresor676 <habimanatresor676@gmail.com>
2025-12-18 16:04:14 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,28 +1,35 @@
-import { auth } from "./firebase.js";
-import { createUser } from "./user.js";
-
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth } from "./firebase-init.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 
 // INSCRIPTION
-export function register(email, password, name) {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((cred) => {
-      return createUser(cred.user.uid, name, email);
-    })
-    .then(() => {
-      window.location.href = "index.html";
-    })
-    .catch(err => alert(err.message));
+export function register(email, password, displayName) {
+    return createUserWithEmailAndPassword(auth, email, password)
+        .then(userCredential => {
+            const user = userCredential.user;
+            // Optionnel : ajouter displayName et placeholder photo dans la DB
+            return user.uid;
+        });
 }
 
 // CONNEXION
 export function login(email, password) {
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      window.location.href = "index.html";
-    })
-    .catch(err => alert(err.message));
+    return signInWithEmailAndPassword(auth, email, password)
+        .then(userCredential => userCredential.user.uid);
+}
+
+// DECONNEXION
+export function logout() {
+    return signOut(auth);
+}
+
+// État en ligne / hors ligne
+import { ref, onDisconnect, set } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+export function setOnlineStatus(uid, online=true) {
+    const statusRef = ref(db, `status/${uid}`);
+    if (online) {
+        set(statusRef, { state: "online", last_changed: Date.now() });
+        onDisconnect(statusRef).set({ state: "offline", last_changed: Date.now() });
+    } else {
+        set(statusRef, { state: "offline", last_changed: Date.now() });
+    }
 }
