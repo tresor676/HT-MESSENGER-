3fa62c1b5113b3b9983352a2c2b5029b44e65840
tresor676 <habimanatresor676@gmail.com>
2025-12-18 16:03:17 +0200

    Update and rename firebase.js to firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,6 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC2WG-135oKY_s6xf5-nBBhxncNV9UayQ0",
@@ -12,6 +13,7 @@ const firebaseConfig = {
   appId: "1:835562519447:web:e4032e73f7601ba3c522d2"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
