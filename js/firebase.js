686595ff7cc037db1e17fc0e6666b0062e82a6e1
tresor676 <habimanatresor676@gmail.com>
2025-12-19 17:23:35 +0200

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2WG-135oKY_s6xf5-nBBhxncNV9UayQ0",
+  authDomain: "tresor-ae58e.firebaseapp.com",
+  databaseURL: "https://tresor-ae58e-default-rtdb.firebaseio.com",
+  projectId: "tresor-ae58e",
+  storageBucket: "tresor-ae58e.firebasestorage.app",
+  messagingSenderId: "835562519447",
+  appId: "1:835562519447:web:e4032e73f7601ba3c522d2"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
