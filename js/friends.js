4634c548eb8e94018de4be29d3f4b9e057b9f36e
tresor676 <habimanatresor676@gmail.com>
2025-12-20 10:51:05 +0200

    Create friends.js

---
+++ js/friends.js
@@ -0,0 +1,7 @@
+// Liste amis et ajout
+const friendsDb = firebase.firestore();
+
+document.getElementById('searchBtn')?.addEventListener('click', () => {
+    const query = document.getElementById('searchUser').value;
+    // TODO: rechercher utilisateurs et envoyer demande
+});
