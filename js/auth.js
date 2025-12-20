e0045d550fdaddc2b576fd27a250d3c6594be25d
tresor676 <habimanatresor676@gmail.com>
2025-12-20 10:48:14 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,21 @@
-import { auth, db } from "./firebase.js";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  updateProfile
-} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+// Authentification signup / login
+const auth = firebase.auth();
 
-loginBtn.onclick = async () => {
-  await signInWithEmailAndPassword(auth, email.value, password.value);
-  location.href = "home.html";
-};
+document.getElementById('signupForm')?.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const profilePic = document.getElementById('profilePic').files[0];
 
-registerBtn.onclick = async () => {
-  const res = await createUserWithEmailAndPassword(auth, email.value, password.value);
-  await updateProfile(res.user, { displayName: name.value });
+    // TODO: créer compte et upload photo dans Storage
+});
 
-  await setDoc(doc(db, "users", res.user.uid), {
-    name: name.value,
-    email: email.value,
-    online: true
-  });
+document.getElementById('loginForm')?.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-  location.href = "home.html";
-};
+    // TODO: connexion utilisateur
+});
